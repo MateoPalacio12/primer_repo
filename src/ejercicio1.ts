0545822e35da1bd9d47628190faf432c637799a3
MateoPalacio12 <104107379+MateoPalacio12@users.noreply.github.com>
2022-04-22 20:08:49 -0300

    ejercicio secuencia hecho

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -1,6 +1,18 @@
-console.log("posteo en facebbok");
-
-console.log("subir una foto");
-console.log("editar foto");
-console.log("añadir un texto");
-console.log("subir posteo");
+console.log("derecha");
+console.log("recojer monton");
+console.log("derecha");
+console.log("recojer monton");
+console.log("derecha");
+console.log("recojer monton");
+console.log("derecha");
+console.log("recojer monton");
+console.log("arriba");
+console.log("recojer monton");
+console.log("arriba");
+console.log("recojer monton");
+console.log("arriba");
+console.log("recojer monton");
+console.log("arriba");
+console.log("recojer monton");
+console.log("arriba");
+console.log("recojer monton");
